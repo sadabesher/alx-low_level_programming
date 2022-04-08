f38fc17518528f0b24b01a1bb7d4a0d00ae45d14
sada besher <sadabesherr@gmail.com>
2022-04-08 01:07:54 -0700

    4put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * Retrun: Always 0 (success)
  */ 
 int main(void)
- {
-	puts("\"Programming is like building a multilingual puzzle");
+{
+        puts("\"Programming is like building a multilingual puzzle");
 	return (0);
- } 
+} 
