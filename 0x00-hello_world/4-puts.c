6702be7eabed61cf794c34a288a12a411f08707b
sada besher <sadabesherr@gmail.com>
2022-04-08 01:03:18 -0700

    4put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - entry point
- * Retrun: Always 0 (success)
- */ 
+* main - entry point
+* Retrun: Always 0 (success)
+*/ 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
