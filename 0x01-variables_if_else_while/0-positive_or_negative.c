a68f23eeacbe42bc8f9c166d8c2116a0d7522aab
sada besher <sadabesherr@gmail.com>
2022-04-11 01:30:44 -0700

    adv

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,17 +9,16 @@
  */
 int main(void)
 {
-     int n;
-     stand(time(0));
-     n = rand() - RAND_MAX / 2;
-     if (n > 0){
-             printf("%i is postive", n);
-     }	
-     else if (n < 0){
-        printf("%i is negative\n", n);
-     }	
-     else{ 
-               printf("%i is zero\n", n);
-     }	
-     return(0);
- }
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n < 0)
+		printf("%i is negative\n", n);
+	else
+		printf("%i is zero\n", n);
+
+	return (0);
+}
