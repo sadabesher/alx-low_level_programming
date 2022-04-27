94d4ec9cfd0c585156f95b41d54c0d03af936ec7
sada besher <sadabesherr@gmail.com>
2022-04-27 01:36:09 -0700

    adv

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,48 +1,13 @@
-
 #include "main.h"
 
 /**
- * is_numerical - check if it is a digit
- * @n: Number
- * Return: If is a number, return 1 else return 0
+ * set_string - sets the value of a pointer to a char
+ * @s: content is to be copied
+ *@to: string
+ *Return: void
  */
-int is_numerical(unsigned int n)
+
+void set_string(char **s, char *to)
 {
-return (n >= '0' &&  n <= '9');
-}
-
-/**
- * _atoi - convert a string to an integer
- *@s: String
- * Return: Return the num
- */
-int _atoi(char *s)
-{
-unsigned int number, i;
-int sign;
-
-sign = 1;
-number = 0;
-
-
-
-for (i = 0; s[i] != '\0'; i++)
-{
-if (is_numerical(s[i]))
-{
-number = (s[i] - 48) + number * 10;
-
-if (s[i + 1] == ' ')
-break;
-}
-else if (s[i] == '-')
-{
-sign *= -1;
-}
-
-}
-
-return (number *sign);
-
-
+	*s = to;
 }
